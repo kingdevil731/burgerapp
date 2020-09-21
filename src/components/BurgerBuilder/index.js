73fd196ad90b39f2bcf91ab5ed01cd6a83e7a452
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2020-09-21 11:44:47 +0200

    base

---
+++ src/components/BurgerBuilder/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import Aux from '../hoc/auxiliar';
+
+const BurgerBuilder = () => (
+  <Aux>
+    <div>Burger</div>
+    <div>Burger Controls</div>
+  </Aux>
+);
+
+export default BurgerBuilder;
